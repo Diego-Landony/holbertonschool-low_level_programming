12cf40f3ea055d41cc86d67d5061ed88663bc465
Diego-Landony <diegolandonylima@gmail.com>
2023-06-21 10:06:35 -0700

    7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*
+*/
 int main(void)
 {
-	int i;
-
-	for (i = 0; i < 16; i++)
-	{
-		if (i < 10)
-			putchar(i + '0'); /* Print digits 0-9 */
-		else
-			putchar(i + 'a' - 10 + '\n'); /* Print lowercase letters a-f */
-	}
-	return (0);
+int i;
+for (i = 15; i >= 0; i--)
+{
+if (i < 10)
+putchar(i + '0'); /* Print digits 0-9 */
+else
+putchar(i + 'a' - 10); /* Print lowercase letters a-f */
+}
+putchar('\n');
+return (0);
 }
