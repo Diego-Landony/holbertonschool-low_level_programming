1026384cce884ec734aa838c1b3e5c1254159ab3
Diego-Landony <diegolandonylima@gmail.com>
2023-06-21 10:56:46 -0700

    102

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i <= 99; i++)
+{
+for (j = i; j <= 99; j++)
+{
+putchar(i / 10 + '0');   /* Print first digit of i */
+putchar(i % 10 + '0');   /* Print second digit of i */
+putchar(' ');            /* Print space */
+putchar(j / 10 + '0');   /* Print first digit of j */
+putchar(j % 10 + '0');   /* Print second digit of j */
+if (i != 99 || j != 99)  /* Add comma and space unless it's the last combination */
+{
