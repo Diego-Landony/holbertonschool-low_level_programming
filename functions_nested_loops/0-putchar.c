bbd7f3006b0d19e604d82beae42f002141bef838
Diego-Landony <diegolandonylima@gmail.com>
2023-06-21 14:18:53 -0700

    0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: 0 (success)
+ */
+
+int main() {
+    printf("_putchar\n");
+    return 0;
+}
