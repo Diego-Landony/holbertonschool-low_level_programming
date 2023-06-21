9fa214d274a1bfcc54f11de60737435bbf8a1a8e
Diego-Landony <diegolandonylima@gmail.com>
2023-06-21 10:42:09 -0700

    100

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 9; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+putchar(i + '0'); /* Print first digit */
+putchar(j + '0'); /* Print second digit */
+if (i != 8 || j != 9) /* Add comma and space unless it's the last combination */
+{
+putchar(','); /* Print comma */
+putchar(' '); /* Print space */
+}
+}
+}
+putchar('\n'); /* Print newline character */
+return 0;
+}
