36dbb5d31a3acc88ada81d715d9376540004e17f
Diego-Landony <diegolandonylima@gmail.com>
2023-06-21 14:42:46 -0700

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#include "main.h"
+#include <main.h>
 
 /**
  * main - Entry point of the program
  *
  * Return: Always 0
  */
-
-int main() {
-    printf("_putchar\n");
-    return 0;
+int main(void)
+{
+printf("_putchar\n");
+return (0);
 }
+
