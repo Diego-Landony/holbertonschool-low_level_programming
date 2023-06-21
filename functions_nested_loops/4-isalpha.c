ab09f6fa3f45ef373054049498b11c14d0d0f503
Diego-Landony <diegolandonylima@gmail.com>
2023-06-21 15:35:19 -0700

    4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -2,6 +2,8 @@
 /**
  * _isalpha - checks if a character is an alphabetic letter
  *
+ * @c: the character to be checked
+ * 
  * Return: 1 if the character is an alphabetic letter, 0 otherwise
  */
 int _isalpha(int c)
