f3b8387452243cf73336298d7f052bfc5ce05097
Diego-Landony <diegolandonylima@gmail.com>
2023-06-21 15:56:07 -0700

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,8 @@
  */
 int main(void)
 {
-	int r;
-
-	r = print_last_digit(98);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
-}
+int r;
+r = print_last_digit(98);
+_putchar('0' + r);
+_putchar('\n');
+return (0);
