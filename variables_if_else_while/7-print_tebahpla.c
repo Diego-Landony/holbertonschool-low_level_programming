c4a2373b7ff416728cda916b23977271cc680814
Diego-Landony <diegolandonylima@gmail.com>
2023-06-21 10:27:43 -0700

    7

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-
 /**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point of the program
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-int i;   
+int i;
 for (i = 'z'; i >= 'a'; i--)
 {
 putchar(i); /* Print lowercase letters in reverse */
 }
-putchar('\n'); /* Print newline character */  
+putchar('\n'); /* Print newline character */
 return (0);
 }
