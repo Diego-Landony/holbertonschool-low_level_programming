75e71810075d057241499394318172f58b1fd6ba
Diego-Landony <diegolandonylima@gmail.com>
2023-06-21 11:03:46 -0700

    102

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
@@ -16,5 +17,14 @@ putchar(i % 10 + '0');   /* Print second digit of i */
 putchar(' ');            /* Print space */
 putchar(j / 10 + '0');   /* Print first digit of j */
 putchar(j % 10 + '0');   /* Print second digit of j */
-if (i != 99 || j != 99)  /* Add comma and space unless it's the last combination */
+if (i != 99 || j != 99)
+/* Add comma and space unless it's the last combination */
 {
+putchar(',');/* Print comma */
+putchar(' ');/* Print space */
+}
+}
+}
+putchar('\n');/* Print newline character */
+return (0);
+}
