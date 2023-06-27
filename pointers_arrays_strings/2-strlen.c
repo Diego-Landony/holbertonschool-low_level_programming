0e6a0816cf6a937b0da7cbc5193fc15ff5a568ab
Diego-Landony <diegolandonylima@gmail.com>
2023-06-27 12:28:30 -0700

    2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: pointer to the string
- *
- * Return: length of the string
- */
+* _strlen - returns the length of a string
+* @s: pointer to the string
+*
+* Return: length of the string
+*/
 int _strlen(char *s)
 {
 int length = 0;
@@ -14,5 +14,5 @@ while (*s != '\0')
 length++;
 s++;
 }
-return length;
+return (length);
 }
