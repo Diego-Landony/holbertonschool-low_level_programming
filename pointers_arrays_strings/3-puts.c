99fd1e2dbe52343c8ff984c07be3100f3520d732
Diego-Landony <diegolandonylima@gmail.com>
2023-06-27 12:48:03 -0700

    3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -6,11 +6,9 @@
 */
 void _puts(char *str)
 {
-int i = 0;
-while (str[i] != '\0')
-{
-_putchar(str[i]);
-i++;
-}
-_putchar('\n');
+        for (; *str != '\0'; str++)
+        {
+                _putchar(*str);
+        }
+        _putchar('\n');
 }
