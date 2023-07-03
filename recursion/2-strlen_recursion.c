e6b45631c6853ebc54d22bee511af688cc1a6f2f
Diego-Landony <diegolandonylima@gmail.com>
2023-07-03 15:50:57 -0700

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
-
 * _strlen_recursion - Returns the length of a string.
 * @s: The string to be measured.
 *
@@ -11,6 +10,7 @@
 int _strlen_recursion(char *s)
 {
 	int longit = 0;
+
 	if (*s)
 	{
 		longit++;
