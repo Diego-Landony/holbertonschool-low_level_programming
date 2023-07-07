dccef02c1fefd965e812b252cf3126ebdb28f869
Diego-Landony <diegolandonylima@gmail.com>
2023-07-07 08:49:55 -0700

    100

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -3,30 +3,33 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+    int cents, coins;
+    int denominations[] = {25, 10, 5, 2, 1};
+    int num_denominations = sizeof(denominations) / sizeof(denominations[0]);
 
-	int cents = atoi(argv[1]);
+    if (argc != 2)
+    {
+        printf("Error\n");
+        return 1;
+    }
 
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+    cents = atoi(argv[1]);
 
-	int coins = 0;
-	int denominations[] = {25, 10, 5, 2, 1};
-	int num_denominations = sizeof(denominations) / sizeof(denominations[0]);
+    if (cents < 0)
+    {
+        printf("0\n");
+        return 0;
+    }
 
-	for (int i = 0; i < num_denominations; i++)
-	{
-		coins += cents / denominations[i];
-		cents %= denominations[i];
-	}
+    coins = 0;
 
-	printf("%d\n", coins);
-	return (0);
+    for (int i = 0; i < num_denominations; i++)
+    {
+        coins += cents / denominations[i];
+        cents %= denominations[i];
+    }
+
+    printf("%d\n", coins);
+    return 0;
 }
+
