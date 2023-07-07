1b796dca95f73a070c35029c9f7c94f3de83be85
Diego-Landony <diegolandonylima@gmail.com>
2023-07-07 08:46:45 -0700

    100

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -1,71 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-/**
- * check_num - check - string there are digit
- * @str: array str
- *
- * Return: Always 0 (Success)
- */
-int check_num(char *str)
-{
-	/*Declaring variables*/
-	unsigned int count;
-
-	count = 0;
-	while (count < strlen(str)) /*count string*/
-
-	{
-		if (!isdigit(str[count])) /*check if str there are digit*/
-		{
-			return (0);
-		}
-
-		count++;
-	}
-	return (1);
-}
-
-/**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * Return: Always 0 (Success)
- */
 
 int main(int argc, char *argv[])
-
 {
-
-	/*Declaring variables*/
-	int count;
-	int str_to_int;
-	int sum = 0;
-
-	count = 1;
-	while (count < argc) /*Goes through the whole array*/
+	if (argc != 2)
 	{
-		if (check_num(argv[count]))
-
-		{
-			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-			sum += str_to_int;
-		}
-
-		/*Condition if one of the number contains symbols that are not digits*/
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-
-		count++;
+		printf("Error\n");
+		return (1);
 	}
 
-	printf("%d\n", sum); /*print sum*/
+	int cents = atoi(argv[1]);
 
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	int coins = 0;
+	int denominations[] = {25, 10, 5, 2, 1};
+	int num_denominations = sizeof(denominations) / sizeof(denominations[0]);
+
+	for (int i = 0; i < num_denominations; i++)
+	{
+		coins += cents / denominations[i];
+		cents %= denominations[i];
+	}
+
+	printf("%d\n", coins);
 	return (0);
 }
-
